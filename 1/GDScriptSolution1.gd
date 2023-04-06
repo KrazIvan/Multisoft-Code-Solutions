59b065211ee402a34581c426ab184ac7821384bd
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-04-06 19:27:54 +0200

    GDScript solution added.

---
+++ 1/GDScriptSolution1.gd
@@ -0,0 +1,17 @@
+extends Node
+
+func openMultisoftUrl():
+	var s = ""
+	const a = "1112031584"
+
+	for i in range(1, len(a)):
+		if int(a[i]) % 2 == int(a[i-1]) % 2:
+			if a[i] > a[i-1]:
+				s += a[i]
+			else:
+				s += a[i-1]
+
+	OS.shell_open("http://www.multisoft.se/" + s)
+
+func _ready() -> void:
+	openMultisoftUrl()
