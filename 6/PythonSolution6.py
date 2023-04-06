56d37d98e37cdc4655d928c0506c883c64e9dc10
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-04-06 02:46:05 +0200

    Create PythonSolution6.py

---
+++ 6/PythonSolution6.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+s = ""
+a = 3
+while not a == 1:
+    s += str(a)
+    if a % 2 == 0:
+        a = int(a/2)
+    else:
+        a = 3*a + 1
+
+webbrowser.open(f"www.multisoft.se/{s}")
