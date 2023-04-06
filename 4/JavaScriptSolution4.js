e45cb99430861ca96e1ae48cc18c11e7aae16522
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-04-06 14:55:50 +0200

    Create JavaScriptSolution4.js

---
+++ 4/JavaScriptSolution4.js
@@ -0,0 +1,18 @@
+var s = "";
+var a = 3;
+while (a != 1) {
+  s += a.toString();
+  if (a % 2 == 0) {
+    a = a/2
+  } else {
+    a = 3 * a + 1
+  }
+}
+
+const url = "www.multisoft.se/" + s;
+const { exec } = require("child_process");
+if (process.platform === "win32") {
+    exec(`start ${url}`);
+  } else if (process.platform === "darwin" || process.platform === "linux") {
+    exec(`open ${url}`);
+  }
