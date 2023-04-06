2a339c275ecfc86f57f64892b29732afd461a81e
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-04-06 02:45:59 +0200

    Create PythonSolution3.py

---
+++ 3/PythonSolution3.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+def fun(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return fun(n - 1) + fun(n - 2)
+
+s = ""
+for i in range(1, 6):
+    s += str(fun(i))
+
+
+webbrowser.open(f"www.multisoft.se/{s}")
