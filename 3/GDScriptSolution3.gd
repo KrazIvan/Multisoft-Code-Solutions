7903f4cfe0ffa8385d1e5038af434a583de151e8
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-04-06 20:43:32 +0200

    Added GDScript solution.

---
+++ 3/GDScriptSolution3.gd
@@ -0,0 +1,16 @@
+extends Node
+
+func openMultisoftUrl3():
+	var s : String = ""
+	const a : Array = [5, 8, 9, 0, 66, 18]
+
+	for i in range(1, len(a)):
+		if int(a[i]) % 2 == 0 and int(a[i-1]) % 3 == 0:
+			s += str(int(a[i]/2))
+		else:
+			s += str(a[i]*2)
+
+	OS.shell_open("www.multisoft.se/" + s)
+
+func _ready() -> void:
+	openMultisoftUrl3()
